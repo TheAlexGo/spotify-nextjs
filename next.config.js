a63601345ec2e82d3cee092bfc1370d2378c2297
Aleksandr Gordeev <alexander.gordeev@vk.team>
2024-01-07 15:59:20 +0300

    CI/CD - подготовка и первая сборка докера

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    output: process.env.BUILD_OUTPUT || 'standalone',
     images: {
         remotePatterns: [
             {
